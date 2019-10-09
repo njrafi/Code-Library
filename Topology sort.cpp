49333cd4cf66899eef7bfb1e334fdbad05dcdafd
Nayeem Jahan Rafi <njrafibd@gmail.com>
2019-10-10 01:58:47 +0600

    Topological sort cycle added

--- Topology sort.cpp
+++ Topology sort.cpp
@@ -1,39 +1,51 @@
-
-#define mxn 1005
-
-vi edge[mxn],ans;
-bool taken[mxn];
+#define mxn 100005
 
 
-void dfs(int p)
+
+// Initialize all
+bool taken[mxn], processing[mxn];
+bool cycle;
+vector<int> ans;
+vector<int> v[mxn];
+
+void dfs(int node)
 {
-        if(!taken[p])
-        {
-                For(i,edge[p].sz)
-                        dfs(edge[p][i]);
-                ans.pb(p);
-                taken[p] = 1;
-        }
+
+    if (processing[node])
+    {
+        cycle = 1;
+        return;
+    }
+    if (taken[node])
+        return;
+    processing[node] = 1;
+    For(i,v[node].sz)
+    {
+        int vv = v[node][i];
+        dfs(vv);
+    }
+    taken[node] = 1;
+    processing[node] = 0;
+    ans.pb(node);
 }
+
+
 
 int main()
 {
-        int m;
-        cin >> m;
-        while(m--)
-        {
-                cin >> a >> b;
-                edge[b].pb(a);
-        }
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        cin >> a >> b;
+        v[b].pb(a);
+    }
 
-        Fre(i,1,n+1)
-                dfs(i);
+    Fre(i, 1, n + 1)
+        dfs(i);
 
-        For(i,ans.sz)
-                cout << ans[i] << " ";
+    For(i, ans.sz)
+            cout << ans[i] << " ";
 
-    
-
-        return 0;
+    return 0;
 }
-
