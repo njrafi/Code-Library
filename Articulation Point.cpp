15e794df0e325a3a01313f1dd6bef6e7752b7c98
Nayeem Jahan Rafi <njrafibd@gmail.com>
2019-11-03 01:37:04 +0600

    Added Articulation Point explanaiton

--- Articulation Point.cpp
+++ Articulation Point.cpp
@@ -1,4 +1,28 @@
+/*
+The idea is to use DFS. In DFS, we follow vertices in tree form called DFS tree.
 
+In DFS tree, a vertex u is parent of another vertex v,
+if v is discovered by u (obviously v is an adjacent of u in graph).
+
+In DFS tree, a vertex u is articulation point if one of the following two conditions is true.
+1) u is root of DFS tree and it has at least two children.
+2) u is not root of DFS tree and it has a child v such that no vertex in subtree rooted with v has a back edge to one of the ancestors (in DFS tree) of u.
+
+We do DFS traversal of given graph with additional code to find out Articulation Points (APs).
+In DFS traversal, we maintain a parent[] array where parent[u] stores parent of vertex u.
+Among the above mentioned two cases, the first case is simple to detect.
+For every vertex, count children.
+If currently visited vertex u is root (parent[u] is NIL) and has more than two children, print it.
+How to handle second case? We maintain an array disc[] to store discovery time of vertices. 
+For every node u, we need to find out the earliest visited vertex (the vertex with minimum discovery time) 
+that can be reached from subtree rooted with u.
+So we maintain an additional array low[] which is defined as follows.
+
+low[u] = min(disc[u], disc[w]) 
+where w is an ancestor of u and there is a back edge from 
+some descendant of u to w.
+
+*/
 #define maxn 10005
 
 /// Holding the articulation points
